420db82e63b8d7a2afb101007c5f7c8b7f20c6ac
Rishi Kothari <itsrishikothari@gmail.com>
2020-10-17 11:05:33 -0400

    startchanneldialog

---
+++ src/features/onboarding/blocks/startChannelDialog.ts
@@ -0,0 +1,50 @@
+import { club_channel } from '../../../config'
+
+const startChannelDialogBlocks = (id: string) => [
+	{
+		type: 'section',
+		text: {
+			type: 'mrkdwn',
+			text: `:partyparrot: :tada: :party_orpheus: :hackclub: Eyyy, welcome to <#${club_channel}>, <@${id}>!`,
+		},
+	},
+	{
+		type: 'context',
+		elements: [
+			{
+				type: 'plain_text',
+				text: 'Are you a member of TFSS Hack Club?',
+				emoji: true,
+			},
+		],
+	},
+	{
+		type: 'actions',
+		elements: [
+			{
+				type: 'button',
+				text: {
+					type: 'plain_text',
+					emoji: true,
+					text: 'Yep!',
+				},
+				style: 'primary',
+				value: id,
+				action_id: 'onboard_hacker',
+			},
+			{
+				type: 'button',
+				text: {
+					type: 'plain_text',
+					emoji: true,
+					text: 'No thanks.',
+				},
+				style: 'danger',
+				value: id,
+				action_id: 'noPost',
+			},
+		],
+	},
+]
+
+export default startChannelDialogBlocks
