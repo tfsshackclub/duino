e768a96381d0b7d5c66af5b584e6bcbff318c551
Rishi Kothari <itsrishikothari@gmail.com>
2020-10-17 12:43:56 -0400

    setup + config

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,12 @@ export const { signing_secret } = process.env
 export const { token } = process.env
 export const { club_channel } = process.env
 export const name = 'Duino'
+
+export const { apiKey } = process.env
+export const { authDomain } = process.env
+export const { databaseURL } = process.env
+export const { projectId } = process.env
+export const { storageBucket } = process.env
+export const { messagingSenderId } = process.env
+export const { appId } = process.env
+export const { measurementId } = process.env
