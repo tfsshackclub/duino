0b03ab6539ada17c5467bfa3a28280e815d6edfa
Rishi Kothari <itsrishikothari@gmail.com>
2020-10-17 12:43:56 -0400

    setup + config

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,46 @@
 import { App } from '@slack/bolt'
-import { signing_secret, token, name } from './config'
+import { initializeApp } from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/storage'
+import {
+	signing_secret,
+	token,
+	name,
+	apiKey,
+	authDomain,
+	databaseURL,
+	projectId,
+	storageBucket,
+	messagingSenderId,
+	appId,
+	measurementId,
+} from './config'
 import * as features from './features/index'
 
 export const app = new App({
 	signingSecret: signing_secret,
 	token,
 })
+
+export const firebase = initializeApp({
+	apiKey,
+	authDomain,
+	databaseURL,
+	projectId,
+	storageBucket,
+	messagingSenderId,
+	appId,
+	measurementId,
+})
+
+export const storage = firebase.storage()
+export const db = firebase.firestore()
 ;(async () => {
-	// Start your app
 	await app.start(process.env.PORT || 3000)
 
 	console.log(`${name} is running! 🔥`)
+
+	console.log('running on:', firebase.name)
 
 	for (const [feature, handler] of Object.entries(features)) {
 		handler(app)
